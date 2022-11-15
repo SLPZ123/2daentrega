4c02ffabf0ea5466b4421ab07c50a49b62e46e3c
sofia lopez <sofi.lopez123@outlook.com.ar>
2022-11-15 10:06:44 -0300

    firstcommit

---
+++ sofia/segundaPreEntrega/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { NavBarComponent } from './core/components/nav-bar/nav-bar.component';
+import { SideMenuComponent } from './core/components/side-menu/side-menu.component';
+import { SharedModule } from './shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    SideMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
